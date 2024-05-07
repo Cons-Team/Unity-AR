fcc3bc9cf92ab688f52726b3022ec186f22db9e5
Sohyun <105468288+NamSoHyun@users.noreply.github.com>
2024-05-07 09:15:57 +0900

    20240507

---
+++ Assets/Scripts/NaviTest.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NaviTest : MonoBehaviour
+{
+    [SerializeField]
+    private Camera topDownCamera;
+    [SerializeField]
+    private GameObject navTargetObject;
+
+    private NavMeshPath path;
+    private LineRenderer line;
+
+    private bool lineToggle = false;
+
+    // 라인 업데이트 간격
+    private float updateInterval = 0.1f; // 0.1초마다 업데이트
+
+    private float timeSinceLastUpdate = 0f;
+
+    private void Start()
+    {
+        path = new NavMeshPath();
+        line = transform.GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        // 라인 업데이트 간격 체크
+        timeSinceLastUpdate += Time.deltaTime;
+        if (timeSinceLastUpdate >= updateInterval)
+        {
+            // 일정 시간이 지난 후에만 라인 업데이트
+            timeSinceLastUpdate = 0f;
+
+            if (lineToggle)
+            {
+                NavMesh.CalculatePath(transform.position, navTargetObject.transform.position, NavMesh.AllAreas, path);
+                line.positionCount = path.corners.Length;
+                line.SetPositions(path.corners);
+                line.enabled = true;
+            }
+        }
+
+        // 터치 입력을 통한 라인 토글
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            lineToggle = !lineToggle;
+        }
+    }
+}
